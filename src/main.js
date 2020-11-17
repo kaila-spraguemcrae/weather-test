1b0446d5fcd027b814e67f2bba497dceaf109cbf
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-11-16 22:37:16 -0800

    updates from monday HW

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
-//import Triangle from './triangle.js'; //export default function Triangle() {}
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js'
+
+function clearFields() { //function to clear all values and appended texts
+  $('#location').val("");
+  $('.showErrors').val("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+}
 
 $(document).ready(function() {
-
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    clearFields();
+    let promise = WeatherService.getWeather(city); //how we make out API call refering to weather-service.js
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
 });
